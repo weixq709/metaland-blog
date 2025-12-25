6f81728f65a08b5a02083c8c233ac78cc642d9fc
weixq709 <weixiaoqiang8311@163.com>
2025-12-25 17:15:09 +0800

    handler统一向上抛出异常交由recover插件处理、panic打印异常堆栈

--- src/middleware/recover.go
+++ src/middleware/recover.go
@@ -1,6 +1,8 @@
 package middleware
 
 import (
+	"runtime/debug"
+
 	"github.com/gin-gonic/gin"
 	"github.com/wxq/metaland-blog/src/response"
 	"github.com/wxq/metaland-blog/src/xzap/logger"
@@ -10,7 +12,7 @@ func Recover() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		defer func() {
 			if cause := recover(); cause != nil {
-				logger.Errorf("[Recovery] panic recovered, %v", cause)
+				logger.Errorf("[Recovery] panic recovered, err: %s\n%s", cause, string(debug.Stack()))
 				response.FailWithMessage(c, "系统错误")
 			}
 		}()
@@ -18,11 +20,9 @@ func Recover() gin.HandlerFunc {
 		c.Next()
 
 		if len(c.Errors) > 0 {
-			for _, err := range c.Errors {
-				logger.Errorf("[Recovery] %+v", err)
-			}
-			response.FailWithMessage(c, "系统错误")
-			return
+			err := c.Errors.Last()
+			logger.Errorf("%s\n%s", err.Error(), string(debug.Stack()))
+			response.FailWithMessage(c, err.Error())
 		}
 	}
 }
