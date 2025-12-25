1a8263c646a485cb211412e76ffaf958295b7bee
weixq709 <weixiaoqiang8311@163.com>
2025-12-25 17:16:31 +0800

    修复query参数无法绑定问题

--- src/entity/entity.go
+++ src/entity/entity.go
@@ -10,12 +10,12 @@ type User struct {
 }
 
 type Article struct {
-	ID         int64     `json:"id" gorm:"primary_key;column:id"`                     // 主键
-	Title      string    `json:"title" gorm:"column:title"`                           // 文章标题
-	UserID     int64     `json:"userId" gorm:"column:user_id"`                        // 用户ID
-	Content    string    `json:"content" gorm:"column:content"`                       // 文章内容
-	CreateTime time.Time `json:"createTime" gorm:"column:create_time;autoCreateTime"` // 创建时间
-	UpdateTime time.Time `json:"updateTime" gorm:"column:update_time;autoUpdateTime"` // 修改时间
+	ID         int64     `json:"id" form:"id" gorm:"primary_key;column:id"`                             // 主键
+	Title      string    `json:"title" form:"title" gorm:"column:title"`                                // 文章标题
+	UserID     int64     `json:"userId" form:"userId" gorm:"column:user_id"`                            // 用户ID
+	Content    string    `json:"content" form:"content" gorm:"column:content"`                          // 文章内容
+	CreateTime time.Time `json:"createTime" form:"createTime" gorm:"column:create_time;autoCreateTime"` // 创建时间
+	UpdateTime time.Time `json:"updateTime" form:"updateTime" gorm:"column:update_time;autoUpdateTime"` // 修改时间
 }
 
 type Comment struct {
